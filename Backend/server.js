cc5ef60f577a3a71edebb54531f1cc5f9847c6ed
shubhambhattacharya-dev <get.input.2025@gmail.com>
2025-08-24 22:44:21 +0530

    server setup

---
+++ Backend/server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app=express();
+
+const port=process.env.PORT || 5000;
+
+app.get("/",(req,res)=>{
+    console.log("shubham");
+    res.send("hii here");
+})
+
+app.listen(port,()=>{
+    console.log(`http://localhost:${port}`);
+})
